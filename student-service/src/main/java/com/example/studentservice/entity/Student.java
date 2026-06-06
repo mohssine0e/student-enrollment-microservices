f993d40affaf412e266d433d807f419d6d733604
mohssine <mohssineechlaihi85@gmail.com>
2026-06-06 23:01:51 +0100

    complete task 3.4 add student cnie unique constraint

--- student-service/src/main/java/com/example/studentservice/entity/Student.java
+++ student-service/src/main/java/com/example/studentservice/entity/Student.java
@@ -15,7 +15,7 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true, length = 32)
     private String cnie;
 
     @Column(nullable = false)
